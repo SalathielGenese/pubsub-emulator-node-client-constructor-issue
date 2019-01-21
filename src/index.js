126fb2cf8943c4295df4b39fe520fcd57a29729f
SalathielGenese <salathielgenese@gmail.com>
2019-01-21 19:56:18 +0100

    log relevant env' vars

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const { PubSub } = require( '@google-cloud/pubsub' );
 
 
 
+
+console.log({
+    GOOGLE_CLOUD_PROJECT: process.env.GOOGLE_CLOUD_PROJECT,
+    PUBSUB_EMULATOR_HOST: process.env.PUBSUB_EMULATOR_HOST,
+});
 ( async () =>
 {
     const pubsub = new PubSub();
 
     await pubsub.topic(
         Date.now().toString( 36 ) ).get({ autoCreate: true });
-})().then( () =>
-{
-    console.log( 'Done...' );
-})
+})();
